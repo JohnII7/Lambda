9e6710af0fa553365f7947387e824d127ea5a0cd
JohnII7 <157798378@qq.com>
2022-08-06 20:53:56 +0800

    lambda练习二，predicate

--- src/main/java/com/john/demo01/Demo01.java
+++ src/main/java/com/john/demo01/Demo01.java
@@ -1,6 +1,7 @@
 package com.john.demo01;
 
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntPredicate;
 
 /**
  * @author John117
@@ -10,12 +11,23 @@ import java.util.function.IntBinaryOperator;
 public class Demo01 {
     public static void main(String[] args) {
 //        new Thread(() -> System.out.println("新线程run方法被执行了")).start();
-        System.out.println(calculateNum(Integer::sum));;
+        System.out.println(calculateNum(Integer::sum));
+
+        printNum(value -> value % 2 == 0);
     }
 
     public static int calculateNum(IntBinaryOperator operator) {
         int a = 10;
         int b = 20;
         return operator.applyAsInt(a, b);
+    }
+
+    public static void printNum(IntPredicate predicate) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i : arr) {
+            if (predicate.test(i)) {
+                System.out.print(i+" ");
+            }
+        }
     }
 }
