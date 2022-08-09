205080cf8f1a528903df214c80a57978f40bf317
JohnII7 <157798378@qq.com>
2022-08-09 17:47:21 +0800

    中间操作之flatMap2

--- src/main/java/com/john/stream/demo/StreamDemo.java
+++ src/main/java/com/john/stream/demo/StreamDemo.java
@@ -1,8 +1,6 @@
 package com.john.stream.demo;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 /**
  * @author John117
@@ -11,7 +9,6 @@ import java.util.stream.Stream;
  */
 public class StreamDemo {
     public static void main(String[] args) {
-        List<Author> authors = getAuthors();
 //        test1(authors);
 //        test2();
 //        test3();
@@ -21,13 +18,25 @@ public class StreamDemo {
 //        test7();
 //        test8();
 //        test9();
-        test10();
+//        test10();
+        test11();
+    }
+
+    private static void test11() {
+        // 打印所有数据的分类, 去重, 以及不能出现哲学爱情的分类
+        getAuthors().stream()
+                .flatMap(author -> author.getBooks().stream())
+                .distinct()
+                .flatMap(book -> Arrays.stream(book.getCategory().split(",")))
+                .distinct()
+                .forEach(System.out::println);
+
     }
 
     private static void test10() {
         // 打印所有书籍的名字，去重
         getAuthors().stream()
-                .flatMap((Function<Author, Stream<Book>>) author -> author.getBooks().stream())
+                .flatMap(author -> author.getBooks().stream())
                 .distinct()
                 .forEach(book -> System.out.println(book.getName()));
     }
@@ -55,12 +64,7 @@ public class StreamDemo {
         // 对年龄降序去重
         getAuthors().stream()
                 .distinct()
-                .sorted(new Comparator<Author>() {
-                    @Override
-                    public int compare(Author o1, Author o2) {
-                        return o2.getAge()-o1.getAge();
-                    }
-                })
+                .sorted((o1, o2) -> o2.getAge()-o1.getAge())
                 .forEach(author -> System.out.println(author.getAge()));
     }
 
@@ -125,12 +129,12 @@ public class StreamDemo {
         List<Book> book3 = new ArrayList<>();
         List<Book> book4 = new ArrayList<>();
 
-        book1.add(new Book(1L, "道德两侧是光明与黑暗", "哲学，爱情", 88, "用一把刀划分了爱恨"));
-        book1.add(new Book(2L, "一个人不能死在同一把刀下", "个人成长、爱情", 99, "讲述如何从失败到成功"));
+        book1.add(new Book(1L, "道德两侧是光明与黑暗", "哲学,爱情", 88, "用一把刀划分了爱恨"));
+        book1.add(new Book(2L, "一个人不能死在同一把刀下", "个人成长,爱情", 99, "讲述如何从失败到成功"));
 
         book1.add(new Book(3L, "那风吹不到的地方", "哲学", 85, "带你用思维去领略世界的尽头"));
         book1.add(new Book(3L, "那风吹不到的地方", "哲学", 85, "带你用思维去领略世界的尽头"));
-        book1.add(new Book(4L, "吹或不吹", "爱情、个人传记", 56, "一个哲学家的爱情观注定很难"));
+        book1.add(new Book(4L, "吹或不吹", "爱情,个人传记", 56, "一个哲学家的爱情观注定很难"));
 
         book1.add(new Book(5L, "你的剑就是我的剑", "爱情", 56, "无法想象一个武者能对他的伴侣这么温柔"));
         book1.add(new Book(6L, "风与剑", "个人传记", 100, "两个哲学家的灵魂和肉体的碰撞会激起怎样的火花"));
@@ -141,7 +145,6 @@ public class StreamDemo {
         author3.setBooks(book3);
         author4.setBooks(book4);
 
-        List<Author> authorList = new ArrayList<>(Arrays.asList(author1, author2, author3, author4));
-        return authorList;
+        return new ArrayList<>(Arrays.asList(author1, author2, author3, author4));
     }
 }
